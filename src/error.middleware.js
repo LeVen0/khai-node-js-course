5bb46ca1308c7871c991983c76b797526822ed0b
Serhii Ponomarenko <sergjoponomar@gmail.com>
2026-05-27 19:38:08 +0300

    setup database connection with drizzle

--- src/error.middleware.js
+++ src/error.middleware.js
@@ -1,6 +1,4 @@
-const errorResponder = (err, request, response, next) => {
+export const errorResponder = (err, request, response, next) => {
     response.header('Content-Type', 'application/json');
     response.status(err.statusCode || 500).send(err.message);
 };
-
-module.exports = { errorResponder };
